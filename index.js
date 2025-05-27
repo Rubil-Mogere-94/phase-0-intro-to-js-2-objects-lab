874fd1445b2725ae4b84b6607f52ca910fe1eaa5
Rubil <mogererubil@gmail.com>
2025-05-27 14:54:42 +0300

    new commit

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+// Global employee object used in tests
+let employee = {
+  name: "Sam",
+  streetAddress: "11 Broadway"
+};
+
+// Non-destructive update: returns a new object with updated key/value
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+// Destructive update: updates original employee object
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Non-destructive delete: returns a clone without the key
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Destructive delete: removes the key directly from the original
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+// Export functions for testing
+module.exports = {
+  employee,
+  updateEmployeeWithKeyAndValue,
+  destructivelyUpdateEmployeeWithKeyAndValue,
+  deleteFromEmployeeByKey,
+  destructivelyDeleteFromEmployeeByKey
+};
